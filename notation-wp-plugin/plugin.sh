8836e3d1870b85d087f4daa2877644ed1600204a
Michael 'Flimmy' Flemming <m.flemming@opencloud.eu>
2025-11-21 15:41:11 +0100

    convert to plugin for secure usage in woodpecker

---
+++ notation-wp-plugin/plugin.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+
+set -e
+
+echo "setup signing keys"
+echo "${PLUGIN_KEY}" > /root/.config/notation/localkeys/oc-ci.key
+echo "${PLUGIN_CRT}" > /root/.config/notation/localkeys/oc-ci.crt
+
+echo "setting up registry logins"
+echo "${PLUGIN_LOGINS}" > /root/tmp.logins.json
+jq 'map({(.registry): {auth: ( (.username + ":" + .password) | @base64 )} }) | add | {auths: .}' /root/tmp.logins.json > /root/.config/notation/config.json
+cp /root/.config/notation/config.json /root/.docker/config.json
+
+echo "getting digest for $PLUGIN_TARGET"
+export DIGEST=$(oras manifest fetch --descriptor registry.heinlein.group/opencloud/notation-wp-plugin:latest | jq -r '.digest')
+
+echo "begin signing"
+
+primary=$(echo ${PLUGIN_TARGET} | sed -e 's/:.*//')
+echo "##### start sign for $primary@$DIGEST #####"
+notation sign $primary@$DIGEST
+
+echo ${PLUGIN_ADDITIONAL} | tr ',' '\n' | while read target
+do
+    set -e
+    echo "##### start sign for $target@$DIGEST #####"
+    notation sign $target@$DIGEST
+done
+
+exit 0
